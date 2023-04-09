32789189a012f8ab2624216f50b5b770dbb07290
Jose Cabrera <murdoc.jose.6@gmail.com>
2023-04-09 18:58:36 -0300

    fix: undo firebase functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import 'firebase/compat/firestore';
-import 'firebase/compat/database';
-import { initializeApp } from 'firebase/app';
-import { getFunctions, httpsCallable } from 'firebase/functions';
-import './App.css';
 
-const app = initializeApp({
-  projectId: "mindco-df9af",
-  apiKey: "AIzaSyCoh8jK7yOCyNPmtRoc-33BQfjeUAeaa7A",
-  authDomain: "mindco-df9af.firebaseapp.com",
-});
-const functions = getFunctions(app);
-const saveUserData = httpsCallable(functions, 'saveUserData', {
-  headers: {
-    "Content-Type": "application/json"
-  }
-});
+import './App.css';
+import saveUserData from './firebase/configFirebase';
 
 function App() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
-  const [messageError, setError] = useState('');
+  const [formMessage, setFormMessage] = useState('');
 
   const clearError = setTimeoutClear(() => {
-    setError('');
-  }, 2000);
+    setFormMessage('');
+  }, 3000);
 
   useEffect(() => {
-    if (messageError !== '') {
+    if (formMessage !== '') {
       clearError();
     }
-  }, [messageError, clearError]);
+  }, [formMessage, clearError]);
 
   function setTimeoutClear(func, time) {
     const timeout = setTimeout(func, time);
@@ -47,7 +33,7 @@ function App() {
     e.preventDefault();
 
     if (!name.trim() || !email.trim() || !phone.trim()) {
-      setError('Error: todos los campos son obligatorios');
+      setFormMessage('Error: todos los campos son obligatorios');
       return;
     }
 
@@ -55,51 +41,47 @@ function App() {
     const phoneRegex = /^[0-9]{10}$/;
 
     if (!emailRegex.test(email)) {
-      setError('Error: el correo electrónico no es válido');
+      setFormMessage('Error: el correo electrónico no es válido');
       return;
     }
 
     if (!phoneRegex.test(phone)) {
-      setError('Error: el número de teléfono no es válido');
+      setFormMessage('Error: el número de teléfono no es válido');
       return;
     }
 
-    const data = {
-      name,
-      email,
-      phone
-    };
-
-    saveUserData(JSON.stringify(data)).then((result) => {
-      console.log(result);
+    try {
+      saveUserData(name, email, phone);
       setName('');
       setEmail('');
       setPhone('');
-    }).catch((error) => {
-      console.log(error);
-      setError('Error: no se pudo guardar la información');
-    });
+      setFormMessage('Se envio correctamente la información');
+    } catch (error) {
+      setFormMessage('Error: no se pudo guardar la información');
+      return;
+    }
+
   }
 
   return (
-    <form onSubmit={handleSubmit} className="container">
-      <div>
-
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      </div>
-      <div>
-        <label htmlFor="phone">Phone:</label>
-        <input type="tel" id="phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
-      </div>
-      <button type="submit">Submit</button>
-
-      {messageError && <p>{messageError}</p>}
-    </form>
+    <>
+      <form onSubmit={handleSubmit} className="container">
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="phone">Phone:</label>
+          <input type="tel" id="phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      {formMessage && <p>{formMessage}</p>}
+    </>
   );
 };
 
